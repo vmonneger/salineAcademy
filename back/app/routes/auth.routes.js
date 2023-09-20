64740663b521d1457fad4d42d0fcd6143595675a
Ismael Traore <ismael.traore@welease.fr>
2023-09-20 21:11:52 +0200

    fix(back): middlewares added to routes

--- back/app/routes/auth.routes.js
+++ back/app/routes/auth.routes.js
@@ -1,13 +1,13 @@
-const { verifySignUp, auth } = require('../middleware');
+const middleware = require('../middleware');
 const controller = require('../controllers/auth/auth.controller');
 
 module.exports = function(app) {
     app.post('/auth/signup',  controller.signUp);
     app.post('/auth/signin', controller.signIn);
-    app.post('/auth/logout', controller.logout);
+    app.post('/auth/logout', [middleware.auth.isLoggedIn], controller.logout);
 
     // A voir si on doit supprimer
     app.get('/auth/check-auth', controller.checkAuth);
   
-    app.post('/auth/update-password',  controller.updatePassword);
+    app.post('/auth/update-password', [middleware.auth.isLoggedIn, middleware.auth.isSameUser], controller.updatePassword);
 };
